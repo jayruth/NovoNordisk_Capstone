d050d8cf716e7d952ce96fdf15760662c1cb12bb
Josh <jsmith07@uw.edu>
2018-06-22 09:37:36 -0700

    Add support for multiclass in train_clstm function.

--- ndac/predict.py
+++ ndac/predict.py
@@ -9,7 +9,7 @@ from keras.layers.convolutional import MaxPooling1D
 from sklearn.model_selection import train_test_split
 
 
-def train_clstm(x, y, test_fraction=0, embedding_len=4,
+def train_clstm(x, y, test_fraction=0, embedding_length=4,
                 batch_size=100, epochs=5, verbose=1,
                 save_file=None):
     # fix random seed for reproducibility
@@ -26,15 +26,22 @@ def train_clstm(x, y, test_fraction=0, embedding_len=4,
     # create the model
     model = Sequential()
     model.add(Embedding(input_dim=vocab_size,
-                        output_dim=embedding_len,
+                        output_dim=embedding_length,
                         input_length=seq_len))
     model.add(Conv1D(filters=128, kernel_size=3, padding='same', activation='selu'))
     model.add(MaxPooling1D(pool_size=2))
     model.add(LSTM(100))
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
+    if np.isscalar(y[0]):
+        model.add(Dense(1, activation='sigmoid'))
+        model.compile(loss='binary_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+    else:
+        model.add(Dense(y.shape[1], activation='softmax'))
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+
     print(model.summary())
     model.fit(x, y, epochs=epochs,
               batch_size=batch_size, verbose=verbose)
