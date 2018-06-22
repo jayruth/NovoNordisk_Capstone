5f6388a45cfc2e08ca860ba471c8ab3a22d5716a
Josh <jsmith07@uw.edu>
2018-06-22 14:32:32 -0700

    Update value_classify function and example notebooks with multiclass.

--- ndac/data_processing.py
+++ ndac/data_processing.py
@@ -7,7 +7,7 @@ from keras.preprocessing import sequence
 from keras.utils import to_categorical
 
 
-def quantile_classify(metric, sequences, quantile_cut, drop_class=None):
+def quantile_classify(metric, sequences, split_quantiles=0.5, drop_class=None):
     """This function creates a new dataframe containing the specified
     metric and sequence and computes a new column, 'class', based on
     the high and low cuts"""
@@ -18,34 +18,35 @@ def quantile_classify(metric, sequences, quantile_cut, drop_class=None):
     # make a histogram of the data to show the locations of the cut points
     hist = dataframe.iloc[:, 0].hist(bins=100)
 
-    if np.isscalar(quantile_cut):
-        cut_val = metric.quantile(quantile_cut)
+    # if a single number is supplied, make high and low classes
+    if np.isscalar(split_quantiles):
+        cut_val = metric.quantile(split_quantiles)
         plt.axvline(cut_val, color='k', linestyle='dashed', linewidth=3)
+        # assign class 1 to upper portion of data
         dataframe.loc[dataframe.iloc[:, 0] > cut_val, ['class']] = 1
         counts = pd.value_counts(dataframe['class'])
         print(counts[1], "samples above high cut,", counts[0],
               "samples below low cut.")
-
         return dataframe, hist
 
-    for idx, cut in enumerate(quantile_cut):
+    for idx, cut in enumerate(split_quantiles):
         # add dashed line for cut point on histogram plot
         high_val = metric.quantile(cut)
         plt.axvline(high_val, linestyle='dashed', linewidth=3, color='k')
         # skip first cut since class = 0 by default
         if idx == 0:
             continue
-        low_val = metric.quantile(quantile_cut[idx - 1])
+
+        low_val = metric.quantile(split_quantiles[idx - 1])
         dataframe.loc[(dataframe.iloc[:, 0] <= high_val)
                       & (dataframe.iloc[:, 0] > low_val),
                       ['class']] = idx
-
-    cut_val = metric.quantile(quantile_cut[-1])
+    # add one more class above the final quantile cut value
+    cut_val = metric.quantile(split_quantiles[-1])
     dataframe.loc[dataframe.iloc[:, 0] > cut_val,
-                  ['class']] = len(quantile_cut)
-    # drop classes
+                  ['class']] = len(split_quantiles)
+    # drop classes if they are supplied
     if drop_class:
-        bool = -dataframe['class'].isin(drop_class)
         dataframe = dataframe[-dataframe['class'].isin(drop_class)]
         # reassign classes based on number of unique remaining classes
         for idx, old_class in enumerate(np.unique(dataframe['class'])):
@@ -60,35 +61,56 @@ def quantile_classify(metric, sequences, quantile_cut, drop_class=None):
     return dataframe, hist
 
 
-def value_classify(metric, sequences, high_value, low_value):
+def value_classify(metric, sequences, split_values, drop_class=None):
     """This function creates a new dataframe containing the specified
     metric and sequence and computes a new column, 'class', based on
     the high and low values specified for metric"""
 
     dataframe = pd.concat([metric, sequences], axis=1)
-
-    # make a histogram of the data to show the locations of the cut points
-    hist = metric.hist(bins=100)
-    plt.axvline(low_value, color='k', linestyle='dashed', linewidth=3)
-    plt.axvline(high_value, color='r', linestyle='dashed', linewidth=3)
-
-    # function to assign class based on high and low cut
-    def assign_class(metric):
-        if metric <= low_value:
-            return 0
-        elif metric >= high_value:
-            return 1
-        return
-
-    # apply to the dataframe then remove values not assigned a class
-    dataframe['class'] = dataframe.iloc[:, 0].apply(assign_class)
-    dataframe = dataframe[pd.notnull(dataframe['class'])]
-
-    counts = pd.value_counts(dataframe['class'])
+    dataframe['class'] = 0
     print(len(metric), "samples input.")
-    print(counts[1], "samples above high value,", counts[0],
-          "samples below low value,", len(metric) - len(dataframe),
-          "samples removed.")
+    # make a histogram of the data to show the locations of the cut points
+    hist = dataframe.iloc[:, 0].hist(bins=100)
+
+    # if a single number is supplied, make high and low classes
+    if np.isscalar(split_values):
+        plt.axvline(split_values, color='k',
+                    linestyle='dashed', linewidth=3)
+        # assign class 1 to upper portion of data
+        dataframe.loc[dataframe.iloc[:, 0] > split_values, ['class']] = 1
+        counts = pd.value_counts(dataframe['class'])
+        print(counts[1], "samples above high cut,", counts[0],
+              "samples below low cut.")
+        return dataframe, hist
+
+    for idx, cut in enumerate(split_values):
+        # add dashed line for cut point on histogram plot
+        high_val = cut
+        plt.axvline(high_val, linestyle='dashed', linewidth=3, color='k')
+        # skip first cut since class = 0 by default
+        if idx == 0:
+            continue
+
+        low_val = split_values[idx - 1]
+        dataframe.loc[(dataframe.iloc[:, 0] <= high_val)
+                      & (dataframe.iloc[:, 0] > low_val),
+                      ['class']] = idx
+    # add one more class above the final quantile cut value
+    cut_val = split_values[-1]
+    dataframe.loc[dataframe.iloc[:, 0] > cut_val,
+                  ['class']] = len(split_values)
+    # drop classes if they are supplied
+    if drop_class:
+        dataframe = dataframe[-dataframe['class'].isin(drop_class)]
+        # reassign classes based on number of unique remaining classes
+        for idx, old_class in enumerate(np.unique(dataframe['class'])):
+            dataframe.loc[dataframe['class'] == old_class, 'class'] = idx
+
+    # get and print sample count for each class
+    counts = pd.value_counts(dataframe['class']).sort_index()
+    for idx, count in counts.iteritems():
+        print(f'{count} samples in class {idx}')
+    print(f'{len(metric) - len(dataframe)} samples removed.')
 
     return dataframe, hist
 
