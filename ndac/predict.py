20b903b2c964a399da6866b8220fc0fc31d6d634
Josh <jsmith07@uw.edu>
2018-09-28 16:53:10 -0700

    Refactor models to suit cross-validation.

--- ndac/predict.py
+++ ndac/predict.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 from keras.models import Sequential
 from keras.layers import Dense
@@ -12,9 +13,9 @@ from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
 
 
-def _clstm(vocab_size, embedding_length, seq_len,
-           cnn_filters, filter_length, x, pool_size,
-           nodes, dropout, lstm_drop):
+def _clstm(categorical=False, vocab_size=False, embedding_length=10,
+           seq_len=200, cnn_filters=128, filter_length=3,
+           pool_size=2, nodes=100, lstm_drop=0.2, dropout=0.5):
     model = Sequential()
     model.add(Embedding(input_dim=vocab_size,
                         output_dim=embedding_length,
@@ -26,15 +27,26 @@ def _clstm(vocab_size, embedding_length, seq_len,
         model.pop()
         model.pop()
         model.add(Conv1D(filters=cnn_filters, kernel_size=filter_length,
-                         input_shape=(x.shape[1], x.shape[2]),
+                         input_shape=(seq_len, vocab_size),
                          padding='same', activation='selu'))
     model.add(MaxPooling1D(pool_size=pool_size))
     model.add(LSTM(nodes, dropout=dropout, recurrent_dropout=lstm_drop))
+    if not categorical:
+        model.add(Dense(1, activation='sigmoid'))
+        model.compile(loss='binary_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+    else:
+        model.add(Dense(categorical, activation='softmax'))
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+
     return model
 
 
-def _lstm(vocab_size, embedding_length, seq_len,
-          nodes, dropout, lstm_drop, x):
+def _lstm(categorical=False, vocab_size=False, embedding_length=10,
+          seq_len=200, nodes=100, lstm_drop=0.2, dropout=0.5):
     model = Sequential()
     model.add(Embedding(input_dim=vocab_size,
                         output_dim=embedding_length,
@@ -46,13 +58,23 @@ def _lstm(vocab_size, embedding_length, seq_len,
         model.pop()
         model.add(LSTM(nodes, dropout=dropout,
                        recurrent_dropout=lstm_drop,
-                       input_shape=(x.shape[1], x.shape[2])))
+                       input_shape=(seq_len, vocab_size)))
+    if not categorical:
+        model.add(Dense(1, activation='sigmoid'))
+        model.compile(loss='binary_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+    else:
+        model.add(Dense(categorical, activation='softmax'))
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
     return model
 
 
-def _cnn(vocab_size, embedding_length, seq_len,
-         cnn_filters, filter_length, x,
-         pool_size, nodes, dropout):
+def _cnn(categorical=False, vocab_size=None, embedding_length=10,
+         seq_len=200, cnn_filters=128, filter_length=3,
+         pool_size=2, nodes=100, dropout=0.5):
     model = Sequential()
     model.add(Embedding(input_dim=vocab_size,
                         output_dim=embedding_length,
@@ -64,63 +86,58 @@ def _cnn(vocab_size, embedding_length, seq_len,
         model.pop()
         model.pop()
         model.add(Conv1D(filters=cnn_filters, kernel_size=filter_length,
-                         input_shape=(x.shape[1], x.shape[2]),
+                         input_shape=(seq_len, vocab_size),
                          padding='same', activation='selu'))
     model.add(MaxPooling1D(pool_size=pool_size))
     model.add(Flatten())
     model.add(Dense(nodes))
     model.add(Dropout(dropout))
-    return model
-
-
-def train_model(x, y, architecture='clstm', test_fraction=0,
-                embedding_length=10, batch_size=100, epochs=5,
-                verbose=1, save_file=None, cnn_filters=128,
-                filter_length=3, pool_size=2, nodes=100,
-                lstm_drop=0.2, dropout=0.5):
-    # fix random seed for reproducibility
-    np.random.seed(7)
-    # get embedding parameters from x matrix
-    vocab_size = x.max() + 1
-    seq_len = x.shape[1]
-
-    if test_fraction:
-        # create test-train split
-        x, x_test, y, y_test = train_test_split(x, y,
-                                                test_size=test_fraction)
-    # convert None embedding value to False to prevent error
-    if embedding_length is None:
-        embedding_length = False
-
-    if not embedding_length:
-        x = to_categorical(x)
-        if test_fraction:
-            x_test = to_categorical(x_test, num_classes=x.shape[-1])
-
-    # create the model
-    # select architecture
-    if architecture == 'clstm':
-        model = _clstm(vocab_size, embedding_length, seq_len,
-                       cnn_filters, filter_length, x, pool_size,
-                       nodes, dropout, lstm_drop)
-    elif architecture == 'lstm':
-        model = _lstm(vocab_size, embedding_length, seq_len,
-                      nodes, dropout, lstm_drop, x)
-    elif architecture == 'cnn':
-        model = _cnn(vocab_size, embedding_length, seq_len,
-                     cnn_filters, filter_length, x,
-                     pool_size, nodes, dropout)
-
-    if np.isscalar(y[0]):
+    if not categorical:
         model.add(Dense(1, activation='sigmoid'))
         model.compile(loss='binary_crossentropy',
                       optimizer='adam',
                       metrics=['accuracy'])
     else:
-        model.add(Dense(y.shape[1], activation='softmax'))
+        model.add(Dense(categorical, activation='softmax'))
         model.compile(loss='categorical_crossentropy',
                       optimizer='adam',
                       metrics=['accuracy'])
+    return model
+
+
+def train_model(x, y, architecture='clstm', test_fraction=0,
+                skip_embedding=False, batch_size=100, epochs=5,
+                verbose=1, save_file=None, **kwargs):
+    # fix random seed for reproducibility
+    np.random.seed(7)
+
+    if test_fraction:
+        # create test-train split
+        x, x_test, y, y_test = train_test_split(
+            x, y, test_size=test_fraction
+        )
+
+    kwargs['vocab_size'] = int(x.max() + 1)
+    # convert None embedding value to False to prevent error
+    if skip_embedding:
+        kwargs['embedding_length'] = False
+        x = to_categorical(x)
+        if test_fraction:
+            x_test = to_categorical(x_test, num_classes=x.shape[-1])
+
+    # get embedding parameters from x matrix
+    kwargs['seq_len'] = int(x.shape[1])
+
+    if not np.isscalar(y[0]):
+        kwargs['categorical'] = y.shape[1]
+
+    # select model architecture
+    if architecture == 'clstm':
+        model = _clstm(**kwargs)
+    elif architecture == 'lstm':
+        model = _lstm(**kwargs)
+    elif architecture == 'cnn':
+        model = _cnn(**kwargs)
 
     print(model.summary())
 
