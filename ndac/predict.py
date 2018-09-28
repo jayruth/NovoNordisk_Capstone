611e7c2d3edf3cf076056dcf51f4f34bd9d461f1
Josh <jsmith07@uw.edu>
2018-09-28 15:10:24 -0700

    add option to train lstm or cnn instead of clstm.

--- ndac/predict.py
+++ ndac/predict.py
@@ -3,6 +3,8 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Flatten
+from keras.layers import Dropout
 from keras.layers.embeddings import Embedding
 from keras.layers.convolutional import Conv1D
 from keras.layers.convolutional import MaxPooling1D
@@ -10,11 +12,72 @@ from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
 
 
-def train_clstm(x, y, test_fraction=0, embedding_length=10,
-                batch_size=100, epochs=5, verbose=1,
-                save_file=None, cnn_filters=128, filter_length=3,
-                pool_size=2, lstm_nodes=100, lstm_drop=0.2,
-                dropout=0.5):
+def _clstm(vocab_size, embedding_length, seq_len,
+           cnn_filters, filter_length, x, pool_size,
+           nodes, dropout, lstm_drop):
+    model = Sequential()
+    model.add(Embedding(input_dim=vocab_size,
+                        output_dim=embedding_length,
+                        input_length=seq_len))
+    model.add(Conv1D(filters=cnn_filters, kernel_size=filter_length,
+                     padding='same', activation='selu'))
+    # if user requests no embedding, replace w/ CNN only
+    if not embedding_length:
+        model.pop()
+        model.pop()
+        model.add(Conv1D(filters=cnn_filters, kernel_size=filter_length,
+                         input_shape=(x.shape[1], x.shape[2]),
+                         padding='same', activation='selu'))
+    model.add(MaxPooling1D(pool_size=pool_size))
+    model.add(LSTM(nodes, dropout=dropout, recurrent_dropout=lstm_drop))
+    return model
+
+
+def _lstm(vocab_size, embedding_length, seq_len,
+          nodes, dropout, lstm_drop, x):
+    model = Sequential()
+    model.add(Embedding(input_dim=vocab_size,
+                        output_dim=embedding_length,
+                        input_length=seq_len))
+    model.add(LSTM(nodes, dropout=dropout, recurrent_dropout=lstm_drop))
+    # if user requests no embedding, replace w/ LSTM only
+    if not embedding_length:
+        model.pop()
+        model.pop()
+        model.add(LSTM(nodes, dropout=dropout,
+                       recurrent_dropout=lstm_drop,
+                       input_shape=(x.shape[1], x.shape[2])))
+    return model
+
+
+def _cnn(vocab_size, embedding_length, seq_len,
+         cnn_filters, filter_length, x,
+         pool_size, nodes, dropout):
+    model = Sequential()
+    model.add(Embedding(input_dim=vocab_size,
+                        output_dim=embedding_length,
+                        input_length=seq_len))
+    model.add(Conv1D(filters=cnn_filters, kernel_size=filter_length,
+                     padding='same', activation='selu'))
+    # if user requests no embedding, replace w/ CNN only
+    if not embedding_length:
+        model.pop()
+        model.pop()
+        model.add(Conv1D(filters=cnn_filters, kernel_size=filter_length,
+                         input_shape=(x.shape[1], x.shape[2]),
+                         padding='same', activation='selu'))
+    model.add(MaxPooling1D(pool_size=pool_size))
+    model.add(Flatten())
+    model.add(Dense(nodes))
+    model.add(Dropout(dropout))
+    return model
+
+
+def train_model(x, y, architecture='clstm', test_fraction=0,
+                embedding_length=10, batch_size=100, epochs=5,
+                verbose=1, save_file=None, cnn_filters=128,
+                filter_length=3, pool_size=2, nodes=100,
+                lstm_drop=0.2, dropout=0.5):
     # fix random seed for reproducibility
     np.random.seed(7)
     # get embedding parameters from x matrix
@@ -26,28 +89,28 @@ def train_clstm(x, y, test_fraction=0, embedding_length=10,
         x, x_test, y, y_test = train_test_split(x, y,
                                                 test_size=test_fraction)
     # convert None embedding value to False to prevent error
-    if embedding_length == None:
+    if embedding_length is None:
         embedding_length = False
 
-    # create the model
-    model = Sequential()
-    model.add(Embedding(input_dim=vocab_size,
-                        output_dim=embedding_length,
-                        input_length=seq_len))
-    model.add(Conv1D(filters=cnn_filters, kernel_size=filter_length,
-                     padding='same', activation='selu'))
-    # if user requests no embedding, replace w/ CNN only
     if not embedding_length:
-        model.pop()
-        model.pop()
         x = to_categorical(x)
-        x_test = to_categorical(x_test, num_classes=x.shape[-1])
-        print(x.shape)
-        model.add(Conv1D(filters=cnn_filters, kernel_size=filter_length,
-                         input_shape=(x.shape[1], x.shape[2]),
-                         padding='same', activation='selu'))
-    model.add(MaxPooling1D(pool_size=pool_size))
-    model.add(LSTM(lstm_nodes, dropout=dropout, recurrent_dropout=lstm_drop))
+        if test_fraction:
+            x_test = to_categorical(x_test, num_classes=x.shape[-1])
+
+    # create the model
+    # select architecture
+    if architecture == 'clstm':
+        model = _clstm(vocab_size, embedding_length, seq_len,
+                       cnn_filters, filter_length, x, pool_size,
+                       nodes, dropout, lstm_drop)
+    elif architecture == 'lstm':
+        model = _lstm(vocab_size, embedding_length, seq_len,
+                      nodes, dropout, lstm_drop, x)
+    elif architecture == 'cnn':
+        model = _cnn(vocab_size, embedding_length, seq_len,
+                     cnn_filters, filter_length, x,
+                     pool_size, nodes, dropout)
+
     if np.isscalar(y[0]):
         model.add(Dense(1, activation='sigmoid'))
         model.compile(loss='binary_crossentropy',
@@ -60,8 +123,18 @@ def train_clstm(x, y, test_fraction=0, embedding_length=10,
                       metrics=['accuracy'])
 
     print(model.summary())
-    model.fit(x, y, epochs=epochs,
-              batch_size=batch_size, verbose=verbose)
+
+    fit_args = {
+        'epochs': epochs,
+        'batch_size': batch_size,
+        'verbose': verbose
+    }
+
+    if test_fraction:
+        fit_args['validation_data'] = (x_test, y_test)
+
+    model.fit(x, y, **fit_args)
+
     # report the test accuracy if we performed a train_test_split
     if test_fraction:
         # Final evaluation of the model
